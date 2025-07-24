5581870bd42b79005b8d75625b256aae22c389f7
17146 <1714673995@qq.com>
2025-07-24 23:18:03 +0800

    添加倒置海嗣文

--- src/main/java/com/apocalypse/caerulaarbor/client/font/ModFont.java
+++ src/main/java/com/apocalypse/caerulaarbor/client/font/ModFont.java
@@ -6,5 +6,5 @@ import net.minecraft.network.chat.Style;
 public class ModFont {
 
     public static final Style SEABORN_LANGUAGE = Style.EMPTY.withFont(CaerulaArborMod.loc("seaborn_language"));
-
+    public static final Style SEABORN_LANGUAGE_INVERTED = Style.EMPTY.withFont(CaerulaArborMod.loc("seaborn_language_inverted"));
 }
--- src/main/java/com/apocalypse/caerulaarbor/item/CaffeineItem.java
+++ src/main/java/com/apocalypse/caerulaarbor/item/CaffeineItem.java
@@ -1,6 +1,7 @@
 
 package com.apocalypse.caerulaarbor.item;
 
+import com.apocalypse.caerulaarbor.client.font.ModFont;
 import net.minecraft.network.chat.Component;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
@@ -11,13 +12,15 @@ import net.minecraft.world.level.Level;
 import java.util.List;
 
 public class CaffeineItem extends Item {
-	public CaffeineItem() {
-		super(new Item.Properties().stacksTo(64).rarity(Rarity.COMMON));
-	}
+    public CaffeineItem() {
+        super(new Item.Properties().stacksTo(64).rarity(Rarity.COMMON));
+    }
 
-	@Override
-	public void appendHoverText(ItemStack itemstack, Level level, List<Component> list, TooltipFlag flag) {
-		super.appendHoverText(itemstack, level, list, flag);
-		list.add(Component.translatable("item.caerula_arbor.caffeine.description_0"));
-	}
+    @Override
+    public void appendHoverText(ItemStack itemstack, Level level, List<Component> list, TooltipFlag flag) {
+        super.appendHoverText(itemstack, level, list, flag);
+        list.add(Component.translatable("item.caerula_arbor.caffeine.description_0"));
+        list.add(Component.literal("BREED SUBSISTING GROW MIGRATION").withStyle(ModFont.SEABORN_LANGUAGE));
+        list.add(Component.literal("BREED SUBSISTING GROW MIGRATION").withStyle(ModFont.SEABORN_LANGUAGE_INVERTED));
+    }
 }
