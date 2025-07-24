4b83b82046b4b8f91f526d22208e11c864dc59b9
17146 <1714673995@qq.com>
2025-07-24 22:47:28 +0800

    添加海嗣文

---
+++ src/main/java/com/apocalypse/caerulaarbor/client/font/ModFont.java
@@ -0,0 +1,10 @@
+package com.apocalypse.caerulaarbor.client.font;
+
+import com.apocalypse.caerulaarbor.CaerulaArborMod;
+import net.minecraft.network.chat.Style;
+
+public class ModFont {
+
+    public static final Style SEABORN_LANGUAGE = Style.EMPTY.withFont(CaerulaArborMod.loc("seaborn_language"));
+
+}
