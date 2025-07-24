9299183a5c42c4012302eded5b92f6114d9fe455
Light_Quanta <zch22248@foxmail.com>
2025-07-25 02:56:55 +0800

    添加英语翻译文本加载

--- src/main/java/com/apocalypse/caerulaarbor/client/font/ModFontHelper.java
+++ src/main/java/com/apocalypse/caerulaarbor/client/font/ModFontHelper.java
@@ -2,16 +2,28 @@ package com.apocalypse.caerulaarbor.client.font;
 
 import com.apocalypse.caerulaarbor.CaerulaArborMod;
 import com.apocalypse.caerulaarbor.config.server.MiscConfig;
+import net.minecraft.client.resources.language.ClientLanguage;
 import net.minecraft.network.chat.Component;
 import net.minecraft.network.chat.HoverEvent;
 import net.minecraft.network.chat.MutableComponent;
 import net.minecraft.network.chat.Style;
+import net.minecraft.server.packs.resources.ResourceManager;
+import net.minecraft.server.packs.resources.SimplePreparableReloadListener;
+import net.minecraft.util.profiling.ProfilerFiller;
+import net.minecraftforge.client.event.RegisterClientReloadListenersEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import org.jetbrains.annotations.NotNull;
 
+import javax.annotation.ParametersAreNonnullByDefault;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Random;
 import java.util.zip.CRC32;
 
+@Mod.EventBusSubscriber(modid = CaerulaArborMod.MODID, bus = Mod.EventBusSubscriber.Bus.MOD)
 public class ModFontHelper {
+    public static ClientLanguage EN_US;
 
     public static final Style SEABORN_LANGUAGE = Style.EMPTY.withFont(CaerulaArborMod.loc("seaborn_language"));
     public static final Style SEABORN_LANGUAGE_INVERTED = Style.EMPTY.withFont(CaerulaArborMod.loc("seaborn_language_inverted"));
@@ -23,6 +35,23 @@ public class ModFontHelper {
         crc32.update(string.getBytes());
         long seed = crc32.getValue();
         return new Random(seed);
+    }
+
+    @SubscribeEvent
+    public static void onResourcePackReload(RegisterClientReloadListenersEvent event) {
+        event.registerReloadListener(new SimplePreparableReloadListener<ClientLanguage>() {
+            @Override
+            @ParametersAreNonnullByDefault
+            protected @NotNull ClientLanguage prepare(ResourceManager pResourceManager, ProfilerFiller pProfiler) {
+                return ClientLanguage.loadFrom(pResourceManager, List.of("en_us"), false);
+            }
+
+            @Override
+            @ParametersAreNonnullByDefault
+            protected void apply(ClientLanguage pObject, ResourceManager pResourceManager, ProfilerFiller pProfiler) {
+                EN_US = pObject;
+            }
+        });
     }
 
     public static MutableComponent randomInvert(String text) {
