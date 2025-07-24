2d7752973cc3b6e3f445e01ed281f86489f7c95b
17146 <1714673995@qq.com>
2025-07-24 19:49:18 +0800

    限制繁育策略

--- src/main/java/com/apocalypse/caerulaarbor/event/LivingEventHandler.java
+++ src/main/java/com/apocalypse/caerulaarbor/event/LivingEventHandler.java
@@ -11,7 +11,10 @@ import com.apocalypse.caerulaarbor.item.relic.IRelic;
 import net.minecraft.core.BlockPos;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.util.Mth;
+import net.minecraft.world.effect.MobEffectInstance;
+import net.minecraft.world.effect.MobEffects;
 import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.MobSpawnType;
 import net.minecraft.world.entity.MobType;
 import net.minecraft.world.entity.ai.attributes.Attribute;
 import net.minecraft.world.entity.ai.attributes.AttributeInstance;
@@ -115,7 +118,7 @@ public class LivingEventHandler {
     public static void onFinalizeSpawn(MobSpawnEvent.FinalizeSpawn event) {
         var entity = event.getEntity();
         handleSanityInjuryResistance(entity);
-        handleSeaBornSpawn(entity);
+        handleSeaBornSpawn(entity, event.getSpawnType());
     }
 
     /**
@@ -143,12 +146,13 @@ public class LivingEventHandler {
     /**
      * 在海嗣刷新时，根据当前的策略，给予不同的属性加成
      */
-    private static void handleSeaBornSpawn(LivingEntity entity) {
+    private static void handleSeaBornSpawn(LivingEntity entity, MobSpawnType type) {
         if (!entity.getType().is(ModTags.EntityTypes.SEA_BORN)) return;
         var level = entity.level();
-        double subsisting = MapVariables.get(level).strategySubsisting;
-        double breed = MapVariables.get(level).strategyBreed;
-        double grow = MapVariables.get(level).strategyGrow;
+        var mapVariables = MapVariables.get(level);
+        int subsisting = mapVariables.strategySubsisting;
+        int breed = mapVariables.strategyBreed;
+        int grow = mapVariables.strategyGrow;
 
         var swimSpeed = entity.getAttribute(Attributes.MOVEMENT_SPEED);
         if (swimSpeed != null) {
@@ -159,6 +163,7 @@ public class LivingEventHandler {
         var maxHealth = entity.getAttribute(Attributes.MAX_HEALTH);
         if (maxHealth != null) {
             maxHealth.addPermanentModifier(new AttributeModifier(CaerulaArborMod.ATTRIBUTE_MODIFIER, 0.3 * subsisting, AttributeModifier.Operation.MULTIPLY_BASE));
+            entity.setHealth(entity.getMaxHealth());
         }
         var armor = entity.getAttribute(Attributes.ARMOR);
         if (armor != null) {
@@ -168,6 +173,9 @@ public class LivingEventHandler {
         if (armorToughness != null) {
             armorToughness.addPermanentModifier(new AttributeModifier(CaerulaArborMod.ATTRIBUTE_MODIFIER, 2 * subsisting, AttributeModifier.Operation.ADDITION));
         }
+        if (subsisting >= 3) {
+            entity.addEffect(new MobEffectInstance(MobEffects.DAMAGE_RESISTANCE, -1, subsisting - 3, false, false));
+        }
 
         // 策略-生长
         var attackDamage = entity.getAttribute(Attributes.ATTACK_DAMAGE);
@@ -176,21 +184,23 @@ public class LivingEventHandler {
         }
 
         // 策略-繁育
-        if (breed > 0 && !entity.getType().is(ModTags.EntityTypes.SEA_BORN_CREATURE) && !entity.getType().is(ModTags.EntityTypes.SEA_BORN_BOSS)) {
-            double random = Math.random();
-            if (random < 0.05 + 0.05 * breed) {
-                var copyEntity = entity.getType().create(level);
-                if (copyEntity != null) {
-                    copyEntity.setPos(entity.getX() + Mth.nextDouble(level.random, -1, 1), entity.getY(), entity.getZ() + Mth.nextDouble(level.random, -1, 1));
-                    level.addFreshEntity(copyEntity);
-                }
-            }
-            if (breed >= 3) {
-                if (random < 0.05 * (breed - 2)) {
+        if (type != MobSpawnType.SPAWN_EGG && type != MobSpawnType.COMMAND && type != MobSpawnType.DISPENSER && type != MobSpawnType.BUCKET) {
+            if (breed > 0 && !entity.getType().is(ModTags.EntityTypes.SEA_BORN_CREATURE) && !entity.getType().is(ModTags.EntityTypes.SEA_BORN_BOSS)) {
+                double random = Math.random();
+                if (random < 0.05 + 0.05 * breed) {
                     var copyEntity = entity.getType().create(level);
                     if (copyEntity != null) {
                         copyEntity.setPos(entity.getX() + Mth.nextDouble(level.random, -1, 1), entity.getY(), entity.getZ() + Mth.nextDouble(level.random, -1, 1));
                         level.addFreshEntity(copyEntity);
+                    }
+                }
+                if (breed >= 3) {
+                    if (random < 0.05 * (breed - 2)) {
+                        var copyEntity = entity.getType().create(level);
+                        if (copyEntity != null) {
+                            copyEntity.setPos(entity.getX() + Mth.nextDouble(level.random, -1, 1), entity.getY(), entity.getZ() + Mth.nextDouble(level.random, -1, 1));
+                            level.addFreshEntity(copyEntity);
+                        }
                     }
                 }
             }
