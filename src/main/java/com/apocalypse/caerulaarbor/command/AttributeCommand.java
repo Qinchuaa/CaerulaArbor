9bc4c71f97e8e0b1358bb5e0ccef985b01efb2e9
17146 <1714673995@qq.com>
2025-07-28 20:20:21 +0800

    合并灯火调整指令

--- src/main/java/com/apocalypse/caerulaarbor/command/AttributeCommand.java
+++ src/main/java/com/apocalypse/caerulaarbor/command/AttributeCommand.java
@@ -1,12 +1,16 @@
 package com.apocalypse.caerulaarbor.command;
 
 import com.apocalypse.caerulaarbor.capability.ModCapabilities;
+import com.mojang.brigadier.arguments.DoubleArgumentType;
 import com.mojang.brigadier.arguments.IntegerArgumentType;
 import com.mojang.brigadier.builder.LiteralArgumentBuilder;
 import net.minecraft.commands.CommandSourceStack;
 import net.minecraft.commands.Commands;
 import net.minecraft.commands.arguments.EntityArgument;
 import net.minecraft.network.chat.Component;
+import net.minecraft.server.level.ServerPlayer;
+
+import java.util.Collection;
 
 public class AttributeCommand {
 
@@ -67,6 +71,54 @@ public class AttributeCommand {
                                     }
                                     return 0;
                                 })))
+                        .then(Commands.literal("light")
+                                .then(Commands.argument("value", DoubleArgumentType.doubleArg(0, 100.0D))
+                                        .executes(arguments -> {
+                                            var players = EntityArgument.getPlayers(arguments, "player");
+                                            double value = DoubleArgumentType.getDouble(arguments, "value");
+                                            setLight(players, value);
+                                            if (players.size() == 1) {
+                                                arguments.getSource().sendSuccess(() -> Component.translatable("command.caerula_arbor.attribute.light.single", players.iterator().next().getDisplayName(), value), true);
+                                            } else {
+                                                arguments.getSource().sendSuccess(() -> Component.translatable("command.caerula_arbor.attribute.light.multiple", players.size(), value), true);
+                                            }
+                                            return 0;
+                                        }))
+                                .then(Commands.argument("state", LowerCamelCaseEnumArgument.enumArgument(LightState.class))
+                                        .executes(arguments -> {
+                                            var players = EntityArgument.getPlayers(arguments, "player");
+                                            var state = arguments.getArgument("state", LightState.class);
+                                            setLight(players, state.value);
+                                            if (players.size() == 1) {
+                                                arguments.getSource().sendSuccess(() -> Component.translatable("command.caerula_arbor.attribute.light.single", players.iterator().next().getDisplayName(), Component.translatable(state.name)), true);
+                                            } else {
+                                                arguments.getSource().sendSuccess(() -> Component.translatable("command.caerula_arbor.attribute.light.multiple", players.size(), Component.translatable(state.name)), true);
+                                            }
+                                            return 0;
+                                        })))
                 );
+    }
+
+    private static void setLight(Collection<ServerPlayer> players, double value) {
+        for (var player : players) {
+            var variables = ModCapabilities.getPlayerVariables(player);
+            variables.light = value;
+            variables.syncPlayerVariables(player);
+        }
+    }
+
+    public enum LightState {
+        LUMINOUS(100, "caerula_arbor.light.luminous"),
+        FLICKERING(85, "caerula_arbor.light.flickering"),
+        DIM(50, "caerula_arbor.light.dim"),
+        EXTINGUISHED(0, "caerula_arbor.light.extinguished");
+
+        public final int value;
+        public final String name;
+
+        LightState(int value, String name) {
+            this.value = value;
+            this.name = name;
+        }
     }
 }
