e3eadf0e45b234f7a08eac632c86b0b6208bd1ec
17146 <1714673995@qq.com>
2025-07-29 23:06:07 +0800

    清理诡核集养者的部分代码

--- src/main/java/com/apocalypse/caerulaarbor/client/model/entity/DivicellularHoarderModel.java
+++ src/main/java/com/apocalypse/caerulaarbor/client/model/entity/DivicellularHoarderModel.java
@@ -1,24 +1,23 @@
 package com.apocalypse.caerulaarbor.client.model.entity;
 
+import com.apocalypse.caerulaarbor.CaerulaArborMod;
 import com.apocalypse.caerulaarbor.entity.DivicellularHoarderEntity;
+import net.minecraft.resources.ResourceLocation;
 import software.bernie.geckolib.model.GeoModel;
 
-import net.minecraft.resources.ResourceLocation;
-
 public class DivicellularHoarderModel extends GeoModel<DivicellularHoarderEntity> {
-	@Override
-	public ResourceLocation getAnimationResource(DivicellularHoarderEntity entity) {
-		return new ResourceLocation("caerula_arbor", "animations/divicellular_hoarder.animation.json");
-	}
+    @Override
+    public ResourceLocation getAnimationResource(DivicellularHoarderEntity entity) {
+        return CaerulaArborMod.loc("animations/divicellular_hoarder.animation.json");
+    }
 
-	@Override
-	public ResourceLocation getModelResource(DivicellularHoarderEntity entity) {
-		return new ResourceLocation("caerula_arbor", "geo/divicellular_hoarder.geo.json");
-	}
+    @Override
+    public ResourceLocation getModelResource(DivicellularHoarderEntity entity) {
+        return CaerulaArborMod.loc("geo/divicellular_hoarder.geo.json");
+    }
 
-	@Override
-	public ResourceLocation getTextureResource(DivicellularHoarderEntity entity) {
-		return new ResourceLocation("caerula_arbor", "textures/entity/" + entity.getTexture() + ".png");
-	}
-
+    @Override
+    public ResourceLocation getTextureResource(DivicellularHoarderEntity entity) {
+        return CaerulaArborMod.loc("textures/entity/divicellular_hoarder.png");
+    }
 }
