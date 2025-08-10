c1b40363c734f74fd9b400838683fb602b60711a
17146 <1714673995@qq.com>
2025-08-11 03:04:23 +0800

    调整观测站模型，添加基本的碰撞箱

--- src/main/java/com/apocalypse/caerulaarbor/block/TideObservationStationBlock.java
+++ src/main/java/com/apocalypse/caerulaarbor/block/TideObservationStationBlock.java
@@ -13,13 +13,42 @@ import net.minecraft.world.level.block.state.StateDefinition;
 import net.minecraft.world.level.block.state.properties.DirectionProperty;
 import net.minecraft.world.level.material.PushReaction;
 import net.minecraft.world.phys.shapes.CollisionContext;
+import net.minecraft.world.phys.shapes.Shapes;
 import net.minecraft.world.phys.shapes.VoxelShape;
 import org.jetbrains.annotations.NotNull;
 
-import javax.annotation.ParametersAreNonnullByDefault;
-
 @SuppressWarnings("deprecation")
 public class TideObservationStationBlock extends Block {
+
+    // TODO 将东西两个方向的碰撞箱设置为正确的值
+    public static final VoxelShape SHAPE_PLATE_NORTH = Block.box(1, 0, 4, 15, 3, 16);
+    public static final VoxelShape SHAPE_PLATE_EAST = Block.box(4, 0, 1, 16, 3, 15);
+    public static final VoxelShape SHAPE_PLATE_SOUTH = Block.box(1, 0, 0, 15, 3, 12);
+    public static final VoxelShape SHAPE_PLATE_WEST = Block.box(0, 0, 1, 4, 3, 15);
+
+    public static final VoxelShape SHAPE_POST_NORTH = Shapes.or(Block.box(5, 3, 7, 11, 16, 10), Block.box(2, 3, 10, 14, 16, 14));
+    public static final VoxelShape SHAPE_POST_EAST = Shapes.or(Block.box(7, 3, 5, 10, 16, 11), Block.box(10, 3, 2, 14, 16, 14));
+    public static final VoxelShape SHAPE_POST_SOUTH = Shapes.or(Block.box(5, 3, 6, 11, 16, 9), Block.box(2, 3, 2, 14, 16, 6));
+    public static final VoxelShape SHAPE_POST_WEST = Shapes.or(Block.box(0, 3, 5, 4, 16, 11), Block.box(2, 3, 2, 14, 16, 14));
+
+    public static final VoxelShape SHAPE_COMMON_NORTH = Shapes.or(SHAPE_PLATE_NORTH, SHAPE_POST_NORTH);
+    public static final VoxelShape SHAPE_COMMON_EAST = Shapes.or(SHAPE_PLATE_EAST, SHAPE_POST_EAST);
+    public static final VoxelShape SHAPE_COMMON_SOUTH = Shapes.or(SHAPE_PLATE_SOUTH, SHAPE_POST_SOUTH);
+    public static final VoxelShape SHAPE_COMMON_WEST = Shapes.or(SHAPE_PLATE_WEST, SHAPE_POST_WEST);
+
+    public static final VoxelShape SHAPE_NORTH = Shapes.or(
+            SHAPE_COMMON_NORTH,
+            Block.box(0, 12, 1, 16, 17, 6),
+            Block.box(0, 14, 6, 16, 19, 11),
+            Block.box(0, 16, 11, 16, 21, 16)
+    );
+
+    public static final VoxelShape SHAPE_SOUTH = Shapes.or(
+            SHAPE_COMMON_SOUTH,
+            Block.box(0, 12, 10, 16, 17, 15),
+            Block.box(0, 14, 5, 16, 19, 10),
+            Block.box(0, 16, 0, 16, 21, 5)
+    );
 
     public static final DirectionProperty FACING = HorizontalDirectionalBlock.FACING;
 
@@ -34,12 +63,6 @@ public class TideObservationStationBlock extends Block {
     }
 
     @Override
-    @ParametersAreNonnullByDefault
-    public @NotNull VoxelShape getShape(BlockState state, BlockGetter world, BlockPos pos, CollisionContext context) {
-        return box(0, 0, 0, 16, 16, 16);
-    }
-
-    @Override
     protected void createBlockStateDefinition(StateDefinition.@NotNull Builder<Block, BlockState> builder) {
         builder.add(FACING);
     }
@@ -47,5 +70,40 @@ public class TideObservationStationBlock extends Block {
     @Override
     public BlockState getStateForPlacement(@NotNull BlockPlaceContext context) {
         return this.defaultBlockState().setValue(FACING, context.getHorizontalDirection().getOpposite());
+    }
+
+    @Override
+    public VoxelShape getOcclusionShape(BlockState pState, BlockGetter pLevel, BlockPos pPos) {
+        return switch (pState.getValue(FACING)) {
+            default -> SHAPE_COMMON_NORTH;
+            case EAST -> SHAPE_COMMON_EAST;
+            case SOUTH -> SHAPE_COMMON_SOUTH;
+            case WEST -> SHAPE_COMMON_WEST;
+        };
+    }
+
+    @Override
+    public boolean useShapeForLightOcclusion(BlockState pState) {
+        return true;
+    }
+
+    @Override
+    public VoxelShape getCollisionShape(BlockState pState, BlockGetter pLevel, BlockPos pPos, CollisionContext pContext) {
+        return switch (pState.getValue(FACING)) {
+            default -> SHAPE_COMMON_NORTH;
+            case EAST -> SHAPE_COMMON_EAST;
+            case SOUTH -> SHAPE_COMMON_SOUTH;
+            case WEST -> SHAPE_COMMON_WEST;
+        };
+    }
+
+    @Override
+    public VoxelShape getShape(BlockState pState, BlockGetter pLevel, BlockPos pPos, CollisionContext pContext) {
+        return switch (pState.getValue(FACING)) {
+            default -> SHAPE_NORTH;
+            case EAST -> SHAPE_COMMON_EAST;
+            case SOUTH -> SHAPE_SOUTH;
+            case WEST -> SHAPE_COMMON_WEST;
+        };
     }
 }
