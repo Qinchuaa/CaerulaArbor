00bc1a2542e92e2a4f6c7dc0f2689d847fa5338b
Cassy-Lee <104408348+Cassy-Lee@users.noreply.github.com>
2025-08-29 22:07:39 -0500

    把海怪不攻击海嗣化玩家的判定方式改为实体选择条件
    33^3闻海嗣味怎么没人改啊

--- src/main/java/com/apocalypse/caerulaarbor/entity/ai/goal/SeaMonsterAttackableTargetGoal.java
+++ src/main/java/com/apocalypse/caerulaarbor/entity/ai/goal/SeaMonsterAttackableTargetGoal.java
@@ -5,30 +5,11 @@ import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.Mob;
 import net.minecraft.world.entity.ai.goal.target.NearestAttackableTargetGoal;
 import net.minecraft.world.entity.player.Player;
-import net.minecraft.world.phys.AABB;
 
 public class SeaMonsterAttackableTargetGoal<T extends LivingEntity> extends NearestAttackableTargetGoal<T> {
-
     public SeaMonsterAttackableTargetGoal(Mob pMob, Class<T> pTargetType, boolean pMustSee, boolean pMustReach) {
-        super(pMob, pTargetType, pMustSee, pMustReach);
-    }
-
-    @Override
-    public boolean canUse() {
-        return super.canUse() && excludeSpecialPlayers();
-    }
-
-    @Override
-    public boolean canContinueToUse() {
-        return super.canContinueToUse() && excludeSpecialPlayers();
-    }
-
-    private boolean excludeSpecialPlayers() {
-        return this.mob.level().getEntitiesOfClass(Player.class, new AABB(this.mob.getOnPos()).inflate(16), e -> true)
-                .stream()
-                .noneMatch(player -> player.getCapability(ModCapabilities.PLAYER_VARIABLE)
-                        .map(cap -> cap.seabornization == 3)
-                        .orElse(false)
-                );
+        super(pMob, pTargetType, 10, pMustSee, pMustReach,
+                livingEntity -> !(livingEntity instanceof Player player)
+                        || ModCapabilities.getPlayerVariables(player).seabornization < 3);
     }
 }
