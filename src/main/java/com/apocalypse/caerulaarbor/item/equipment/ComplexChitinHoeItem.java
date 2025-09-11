21e35bc452414be39d2170f583fa35735ff6654c
17146 <1714673995@qq.com>
2025-09-11 21:12:39 +0800

    调整复合几丁质武器属性

--- src/main/java/com/apocalypse/caerulaarbor/item/equipment/ComplexChitinHoeItem.java
+++ src/main/java/com/apocalypse/caerulaarbor/item/equipment/ComplexChitinHoeItem.java
@@ -1,18 +1,24 @@
 package com.apocalypse.caerulaarbor.item.equipment;
 
-import com.apocalypse.caerulaarbor.capability.sanity.SIHelper;
+import com.apocalypse.caerulaarbor.CaerulaArborMod;
+import com.apocalypse.caerulaarbor.init.ModAttributes;
 import com.apocalypse.caerulaarbor.init.ModBlocks;
 import com.apocalypse.caerulaarbor.tiers.ModItemTier;
+import com.google.common.collect.HashMultimap;
+import com.google.common.collect.Multimap;
 import net.minecraft.ChatFormatting;
 import net.minecraft.network.chat.Component;
 import net.minecraft.world.InteractionResult;
-import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.EquipmentSlot;
+import net.minecraft.world.entity.ai.attributes.Attribute;
+import net.minecraft.world.entity.ai.attributes.AttributeModifier;
 import net.minecraft.world.item.*;
 import net.minecraft.world.item.context.UseOnContext;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Blocks;
 
 import java.util.List;
+import java.util.UUID;
 
 public class ComplexChitinHoeItem extends HoeItem {
 
@@ -21,9 +27,15 @@ public class ComplexChitinHoeItem extends HoeItem {
     }
 
     @Override
-    public boolean hurtEnemy(ItemStack itemstack, LivingEntity entity, LivingEntity pAttacker) {
-        SIHelper.causeSanityInjuryWithParticles(entity, pAttacker, 80);
-        return super.hurtEnemy(itemstack, entity, pAttacker);
+    public Multimap<Attribute, AttributeModifier> getDefaultAttributeModifiers(EquipmentSlot pEquipmentSlot) {
+        var map = super.getDefaultAttributeModifiers(pEquipmentSlot);
+        UUID uuid = new UUID(pEquipmentSlot.toString().hashCode(), 0);
+        if (pEquipmentSlot == EquipmentSlot.MAINHAND) {
+            map = HashMultimap.create(map);
+            map.put(ModAttributes.SANITY_INJURY_DAMAGE.get(),
+                    new AttributeModifier(uuid, CaerulaArborMod.ATTRIBUTE_MODIFIER, 80, AttributeModifier.Operation.ADDITION));
+        }
+        return map;
     }
 
     @Override
--- src/main/java/com/apocalypse/caerulaarbor/item/equipment/ComplexChitinPickaxeItem.java
+++ src/main/java/com/apocalypse/caerulaarbor/item/equipment/ComplexChitinPickaxeItem.java
@@ -1,14 +1,20 @@
 package com.apocalypse.caerulaarbor.item.equipment;
 
-import com.apocalypse.caerulaarbor.capability.sanity.SIHelper;
+import com.apocalypse.caerulaarbor.CaerulaArborMod;
+import com.apocalypse.caerulaarbor.init.ModAttributes;
 import com.apocalypse.caerulaarbor.tiers.ModItemTier;
+import com.google.common.collect.HashMultimap;
+import com.google.common.collect.Multimap;
 import net.minecraft.ChatFormatting;
 import net.minecraft.network.chat.Component;
-import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.EquipmentSlot;
+import net.minecraft.world.entity.ai.attributes.Attribute;
+import net.minecraft.world.entity.ai.attributes.AttributeModifier;
 import net.minecraft.world.item.*;
 import net.minecraft.world.level.Level;
 
 import java.util.List;
+import java.util.UUID;
 
 public class ComplexChitinPickaxeItem extends PickaxeItem {
 
@@ -17,9 +23,15 @@ public class ComplexChitinPickaxeItem extends PickaxeItem {
     }
 
     @Override
-    public boolean hurtEnemy(ItemStack itemstack, LivingEntity entity, LivingEntity pAttacker) {
-        SIHelper.causeSanityInjuryWithParticles(entity, pAttacker, 80);
-        return super.hurtEnemy(itemstack, entity, pAttacker);
+    public Multimap<Attribute, AttributeModifier> getDefaultAttributeModifiers(EquipmentSlot pEquipmentSlot) {
+        var map = super.getDefaultAttributeModifiers(pEquipmentSlot);
+        UUID uuid = new UUID(pEquipmentSlot.toString().hashCode(), 0);
+        if (pEquipmentSlot == EquipmentSlot.MAINHAND) {
+            map = HashMultimap.create(map);
+            map.put(ModAttributes.SANITY_INJURY_DAMAGE.get(),
+                    new AttributeModifier(uuid, CaerulaArborMod.ATTRIBUTE_MODIFIER, 80, AttributeModifier.Operation.ADDITION));
+        }
+        return map;
     }
 
     @Override
