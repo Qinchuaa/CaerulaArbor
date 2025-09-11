d74abdb8c5ef25f52c18c728df55bcb438766649
17146 <1714673995@qq.com>
2025-09-11 17:08:44 +0800

    删除一部分procedure

--- src/main/java/com/apocalypse/caerulaarbor/block/NetherseaBlockBlock.java
+++ src/main/java/com/apocalypse/caerulaarbor/block/NetherseaBlockBlock.java
@@ -1,16 +1,17 @@
-
 package com.apocalypse.caerulaarbor.block;
 
 import com.apocalypse.caerulaarbor.capability.ModCapabilities;
 import com.apocalypse.caerulaarbor.capability.sanity.SIHelper;
-import com.apocalypse.caerulaarbor.procedures.GiveTrailBuffProcedure;
+import com.apocalypse.caerulaarbor.init.ModMobEffects;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
 import net.minecraft.core.particles.ParticleTypes;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.util.Mth;
 import net.minecraft.util.RandomSource;
+import net.minecraft.world.effect.MobEffectInstance;
 import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.Level;
@@ -26,58 +27,64 @@ import net.minecraftforge.common.IPlantable;
 import javax.annotation.ParametersAreNonnullByDefault;
 
 public class NetherseaBlockBlock extends Block {
-	public NetherseaBlockBlock() {
-		super(BlockBehaviour.Properties.of().sound(SoundType.SCULK).strength(4f, 6f).lightLevel(s -> 1).friction(0.7f).speedFactor(0.7f).jumpFactor(0.7f));
-	}
+    public NetherseaBlockBlock() {
+        super(BlockBehaviour.Properties.of().sound(SoundType.SCULK).strength(4f, 6f).lightLevel(s -> 1).friction(0.7f).speedFactor(0.7f).jumpFactor(0.7f));
+    }
 
-	@Override
-	public float[] getBeaconColorMultiplier(BlockState state, LevelReader world, BlockPos pos, BlockPos beaconPos) {
-		return new float[]{0.2509803922f, 0.3215686275f, 0.3921568627f};
-	}
+    @Override
+    public float[] getBeaconColorMultiplier(BlockState state, LevelReader world, BlockPos pos, BlockPos beaconPos) {
+        return new float[]{0.2509803922f, 0.3215686275f, 0.3921568627f};
+    }
 
-	@Override
-	@ParametersAreNonnullByDefault
-	public int getLightBlock(BlockState state, BlockGetter worldIn, BlockPos pos) {
-		return 8;
-	}
+    @Override
+    @ParametersAreNonnullByDefault
+    public int getLightBlock(BlockState state, BlockGetter worldIn, BlockPos pos) {
+        return 8;
+    }
 
-	@Override
-	public int getFlammability(BlockState state, BlockGetter world, BlockPos pos, Direction face) {
-		return 1;
-	}
+    @Override
+    public int getFlammability(BlockState state, BlockGetter world, BlockPos pos, Direction face) {
+        return 1;
+    }
 
-	@Override
-	@ParametersAreNonnullByDefault
-	public boolean canSustainPlant(BlockState state, BlockGetter world, BlockPos pos, Direction direction, IPlantable plantable) {
-		return true;
-	}
+    @Override
+    @ParametersAreNonnullByDefault
+    public boolean canSustainPlant(BlockState state, BlockGetter world, BlockPos pos, Direction direction, IPlantable plantable) {
+        return true;
+    }
 
-	@Override
-	public boolean onDestroyedByPlayer(BlockState blockstate, Level world, BlockPos pos, Player entity, boolean willHarvest, FluidState fluid) {
-		double x = pos.getX();
-		double y = pos.getY();
-		double z = pos.getZ();
-		if (entity != null) {
-			if ((ModCapabilities.getPlayerVariables(entity)).seabornization < 2.85) {
-				SIHelper.causeSanityInjury(entity, Mth.nextInt(RandomSource.create(), 32, 96));
-			}
-			if ((LevelAccessor) world instanceof ServerLevel _level)
-				_level.sendParticles(ParticleTypes.ELECTRIC_SPARK, (x + 0.5), (y + 0.5), (z + 0.5), 16, 0.75, 0.75, 0.75, 0.1);
-		}
-		return super.onDestroyedByPlayer(blockstate, world, pos, entity, willHarvest, fluid);
-	}
+    @Override
+    public boolean onDestroyedByPlayer(BlockState blockstate, Level world, BlockPos pos, Player entity, boolean willHarvest, FluidState fluid) {
+        double x = pos.getX();
+        double y = pos.getY();
+        double z = pos.getZ();
+        if (entity != null) {
+            if ((ModCapabilities.getPlayerVariables(entity)).seabornization < 2.85) {
+                SIHelper.causeSanityInjury(entity, Mth.nextInt(RandomSource.create(), 32, 96));
+            }
+            if ((LevelAccessor) world instanceof ServerLevel _level)
+                _level.sendParticles(ParticleTypes.ELECTRIC_SPARK, (x + 0.5), (y + 0.5), (z + 0.5), 16, 0.75, 0.75, 0.75, 0.1);
+        }
+        return super.onDestroyedByPlayer(blockstate, world, pos, entity, willHarvest, fluid);
+    }
 
-	@Override
-	@ParametersAreNonnullByDefault
-	public void entityInside(BlockState blockstate, Level world, BlockPos pos, Entity entity) {
-		GiveTrailBuffProcedure.execute(entity);
-		super.entityInside(blockstate, world, pos, entity);
-	}
+    @Override
+    @ParametersAreNonnullByDefault
+    public void entityInside(BlockState blockstate, Level world, BlockPos pos, Entity entity) {
+        super.entityInside(blockstate, world, pos, entity);
 
-	@Override
-	@ParametersAreNonnullByDefault
-	public void stepOn(Level world, BlockPos pos, BlockState blockstate, Entity entity) {
-		GiveTrailBuffProcedure.execute(entity);
-		super.stepOn(world, pos, blockstate, entity);
-	}
+        if (!(entity instanceof LivingEntity livingEntity)) return;
+        if (livingEntity.level().isClientSide()) return;
+        livingEntity.addEffect(new MobEffectInstance(ModMobEffects.TRAIL_BUFF.get(), 40, 0, false, false));
+    }
+
+    @Override
+    @ParametersAreNonnullByDefault
+    public void stepOn(Level world, BlockPos pos, BlockState blockstate, Entity entity) {
+        super.stepOn(world, pos, blockstate, entity);
+
+        if (!(entity instanceof LivingEntity livingEntity)) return;
+        if (livingEntity.level().isClientSide()) return;
+        livingEntity.addEffect(new MobEffectInstance(ModMobEffects.TRAIL_BUFF.get(), 40, 0, false, false));
+    }
 }
--- src/main/java/com/apocalypse/caerulaarbor/block/OceanFarmlandBlock.java
+++ src/main/java/com/apocalypse/caerulaarbor/block/OceanFarmlandBlock.java
@@ -1,9 +1,10 @@
-
 package com.apocalypse.caerulaarbor.block;
 
-import com.apocalypse.caerulaarbor.procedures.GiveTrailBuffProcedure;
+import com.apocalypse.caerulaarbor.init.ModMobEffects;
 import net.minecraft.core.BlockPos;
+import net.minecraft.world.effect.MobEffectInstance;
 import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
@@ -15,33 +16,36 @@ import net.minecraft.world.phys.shapes.Shapes;
 import net.minecraft.world.phys.shapes.VoxelShape;
 
 public class OceanFarmlandBlock extends Block {
-	public OceanFarmlandBlock() {
-		super(BlockBehaviour.Properties.of().sound(SoundType.GRAVEL).strength(0.5f).speedFactor(0.8f).jumpFactor(0.8f).noOcclusion().isRedstoneConductor((bs, br, bp) -> false));
-	}
+    public OceanFarmlandBlock() {
+        super(BlockBehaviour.Properties.of().sound(SoundType.GRAVEL).strength(0.5f).speedFactor(0.8f).jumpFactor(0.8f).noOcclusion().isRedstoneConductor((bs, br, bp) -> false));
+    }
 
-	@Override
-	public boolean propagatesSkylightDown(BlockState state, BlockGetter reader, BlockPos pos) {
-		return true;
-	}
+    @Override
+    public boolean propagatesSkylightDown(BlockState state, BlockGetter reader, BlockPos pos) {
+        return true;
+    }
 
-	@Override
-	public int getLightBlock(BlockState state, BlockGetter worldIn, BlockPos pos) {
-		return 0;
-	}
+    @Override
+    public int getLightBlock(BlockState state, BlockGetter worldIn, BlockPos pos) {
+        return 0;
+    }
 
-	@Override
-	public VoxelShape getVisualShape(BlockState state, BlockGetter world, BlockPos pos, CollisionContext context) {
-		return Shapes.empty();
-	}
+    @Override
+    public VoxelShape getVisualShape(BlockState state, BlockGetter world, BlockPos pos, CollisionContext context) {
+        return Shapes.empty();
+    }
 
-	@Override
-	public VoxelShape getShape(BlockState state, BlockGetter world, BlockPos pos, CollisionContext context) {
-		return box(0, 0, 0, 16, 15, 16);
-	}
+    @Override
+    public VoxelShape getShape(BlockState state, BlockGetter world, BlockPos pos, CollisionContext context) {
+        return box(0, 0, 0, 16, 15, 16);
+    }
 
-	@Override
-	public void entityInside(BlockState blockstate, Level world, BlockPos pos, Entity entity) {
-		super.entityInside(blockstate, world, pos, entity);
-		GiveTrailBuffProcedure.execute(entity);
-	}
+    @Override
+    public void entityInside(BlockState blockstate, Level world, BlockPos pos, Entity entity) {
+        super.entityInside(blockstate, world, pos, entity);
+
+        if (!(entity instanceof LivingEntity livingEntity)) return;
+        if (livingEntity.level().isClientSide()) return;
+        livingEntity.addEffect(new MobEffectInstance(ModMobEffects.TRAIL_BUFF.get(), 40, 0, false, false));
+    }
 }
